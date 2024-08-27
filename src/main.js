c0e7569064fbe9a3356e139466dec956b40e2144
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-27 12:35:20 +0200

    Funciones

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
 import "./style.css";
+
+//f(x) = 2x
+
+function doble(x){
+    const valor=2
+    console.log("Ejecutando doble")
+    return x*valor;
+};
+
+
+const eldoblededos= doble(2);
+
+console.log(eldoblededos);
+
+console.log(doble(3));
+
+console.log(doble(4));
+
+function sum(x, y){
+    console.log("Ejecutando suma")
+    return x + y;
+};
+
+console.log(sum(1,2));
