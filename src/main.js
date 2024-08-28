90bc7f172121d7ceab654929cc6b3ea5fed82aaf
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-28 11:48:45 +0200

    Sumando

--- src/main.js
+++ src/main.js
@@ -1,36 +1,13 @@
 import "./style.css";
 
-const alubias = "🫘 Alubias";
-const aguacate = "🥑 Aguacate";
-const huevo = "🥚 huevo";
-const cebolla = "🧅 Cebolla";
-const tomate = "🍅 Tomate";
-const lechuga = "🥬 Lechuga";
+function sumar() {
+  const sumando1 = document.getElementById("sumando1").value;
+  const sumando2 = document.getElementById("sumando2").value;
 
-function receta(título, ingredientes, preparación){
-    
-    const estiloTitulo = "color: red; font-size: 18px; font-style: italic;";
-    
-    //Título
-    console.log(`%c${título}`, estiloTitulo);
+  const resultados = parseInt(sumando1) + parseInt(sumando2);
 
-    //Ingredientes
-    console.log(`Ingredientes: ${ingredientes}`);
-    
-    //Preparación
-    console.log(`Preparación: ${preparación}`)
-
-    console.log("*********************************************************");
+  document.getElementById("resultado").innerHTML = resultados;
 }
 
-receta(
-    "Ensalada de alubias con aguacate y huevo",
-    `${alubias}, ${aguacate}, ${huevo}, ${cebolla}, ${tomate}, ${lechuga}.`,
-    "cocer las alubias y dejar enfriar. Mezclar con aguacate en cubos, huevo cocido en rodajas, cebolla en juliana, tomate en cubos y lechuga troceada. Aliñar con aceite de oliva y vinagre."
-);
-
-receta(
-    "Ensalada de setas y bacon",
-    `setas, bacon, ${lechuga}, ${tomate}, ${cebolla}, queso.`,
-    "saltear las setas y el bacon hasta que estén dorados. Mezclar con lechuga troceada, tomate en cubos, cebolla en juliana y queso rallado. Aliñar con aceite de oliva y vinagre."
-);
+const botonSumar = document.getElementById("sumar");
+botonSumar.addEventListener("click", sumar);
