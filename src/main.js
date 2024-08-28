0ca1f38205b364e381f3f522088226e45307b38b
Nacho Diez <diezgarcianacho@gmail.com>
2024-08-28 12:28:10 +0200

    Saludo arreglado :D

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ function saludo() {
   const Nombre = document.getElementById("nombre").value;
   const Apellido = document.getElementById("apellido").value;
 
-  const resultados = "Hola mi nombre es " + Nombre + " " + Apellido;
+  const resultados = `Hola mi nombre es ${Nombre} ${Apellido}`;
 
   document.getElementById("resultado").innerHTML = resultados;
 }
 
-const botonSumar = document.getElementById("saludar");
-botonSumar.addEventListener("click", saludo);
+const botonSaludar = document.getElementById("saludar");
+botonSaludar.addEventListener("click", saludo);
