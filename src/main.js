c812e1b7edce694b8a2919237795415d07ee0591
Nacho Diez <diezgarcianacho@gmail.com>
2024-09-01 19:46:30 +0200

    Hecho otra vez de cero

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import "./style.css";
 
-function cambiarFotoPrincipal(x) {
-  const fotoPrincipal = document.getElementById("foto-principal");
-  const elementoMiniaturaImagen = document.getElementById(x);
+function cambiarFotoPrincipal(LoQuePongoAbajo) {
+  const Foto1 = document.getElementById("foto-principal");
+  const Miniaturas = document.getElementById(LoQuePongoAbajo);
 
-  const auxiliar = fotoPrincipal.src;
+  const Auxiliar = Foto1.src;
+  const Auxiliar2 = Foto1.alt;
 
-  fotoPrincipal.src = elementoMiniaturaImagen.src;
-  fotoPrincipal.alt = elementoMiniaturaImagen.alt;
+  Foto1.src = Miniaturas.src;
+  Foto1.alt = Miniaturas.alt;
 
-  elementoMiniaturaImagen.src = auxiliar;
+  Miniaturas.src = Auxiliar;
+  Miniaturas.alt = Auxiliar2.alt;
 };
-
-
 
 const miniatura1= document.getElementById("miniatura1");
 const miniatura2= document.getElementById("miniatura2");
@@ -20,9 +20,6 @@ const miniatura3= document.getElementById("miniatura3");
 const miniatura4= document.getElementById("miniatura4");
 
 miniatura1.addEventListener("click", () => cambiarFotoPrincipal("miniatura1"));
-
 miniatura2.addEventListener("click", () => cambiarFotoPrincipal("miniatura2"));
-
 miniatura3.addEventListener("click", () => cambiarFotoPrincipal("miniatura3"));
-
-miniatura4.addEventListener ("click",() => cambiarFotoPrincipal("miniatura4"))
+miniatura4.addEventListener("click", () => cambiarFotoPrincipal("miniatura4"));
