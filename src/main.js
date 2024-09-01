efb47c187ad3c2075a11103b84f24a27a4126adc
Nacho Diez <diezgarcianacho@gmail.com>
2024-09-01 18:35:19 +0200

    Siguiente

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import "./style.css";
 
-function saludo() {
-  const Nombre = document.getElementById("nombre").value;
-  const Apellido = document.getElementById("apellido").value;
-
-  const resultados = `Hola mi nombre es ${Nombre} ${Apellido}`;
-
-  document.getElementById("resultado").innerHTML = resultados;
+function cambiarFotoPrincipal(x) {
+  const fotoPrincipal = document.getElementById("foto-principal");
+  const elementoMiniaturaImagen = document.getElementById(x);
+  fotoPrincipal.src = elementoMiniaturaImagen.src;
 }
+const miniatura1= document.getElementById("miniatura1");
+const miniatura2= document.getElementById("miniatura2");
+const miniatura3= document.getElementById("miniatura3");
 
-const botonSaludar = document.getElementById("saludar");
-botonSaludar.addEventListener("click", saludo);
+miniatura1.addEventListener("click", function () {
+  cambiarFotoPrincipal("miniatura1");
+});
+miniatura2.addEventListener("click", function () {
+  cambiarFotoPrincipal("miniatura2");
+});
+miniatura3.addEventListener("click", function(){
+  cambiarFotoPrincipal("miniatura3")
+});
